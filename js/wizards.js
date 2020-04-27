7dc31505dba4fd1565c7b6cf584cd7cf595f577d
Hector-1911 <fontas.chrome@gmail.com>
2020-04-27 19:52:29 +0300

    Модуль приема, отправки данных done

--- js/wizards.js
+++ js/wizards.js
@@ -9,7 +9,16 @@
     };
 
     var onError = function (message) {
-        console.error(message);
+        var node = document.createElement('div');
+
+        node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+        node.style.position = 'absolute';
+        node.style.left = 0;
+        node.style.right = 0;
+        node.style.fontSize = '30px';
+
+        node.textContent = message;
+        document.body.insertAdjacentElement('afterbegin', node);
     };
 
     var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
